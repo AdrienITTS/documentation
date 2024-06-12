d2b9cb4860ee352fd8342dceb8403495a571f1c8
Adrien Deprez <adrien.deprez.fr@gmail.com>
2024-06-12 23:47:11 +0200

    feat(hope): switch to hope plugin

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,62 +1,36 @@
-import { defaultTheme } from '@vuepress/theme-default'
-import { defineUserConfig } from 'vuepress'
-import { viteBundler } from '@vuepress/bundler-vite'
-import { searchPlugin } from "@vuepress/plugin-search";
-import { mdEnhancePlugin } from "vuepress-plugin-md-enhance";
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
 
 export default defineUserConfig({
-  plugins: [
-    searchPlugin({
-      locales: {
-        '/': {
-          placeholder: 'Search',
-        }
-      }
-    }),
-    mdEnhancePlugin({
-      // Enable Align
-      align: true,
-    }),
-  ],
+  base: "/",
 
-  lang: 'en-US',
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Docs Demo",
+      description: "A docs demo for vuepress-theme-hope",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "文档演示",
+      description: "vuepress-theme-hope 的文档演示",
+    },
+  },
 
-  title: 'AdrienITTS Documentation',
+  theme,
 
-  description: 'Website to centralize and share my knowledge about IT',
-
-  theme: defaultTheme({
-    logoDark: '/images/logo-1.svg',
-    logo: '/images/logo-1.svg',
-
-    navbar: ['/', '/who', '/documentation/'],
-
-    sidebar: [
-      // SidebarItem
-      {
-        text: 'Foo',
-        prefix: '/foo/',
-        link: '/foo/',
-        children: [
-          // SidebarItem
-          {
-            text: 'github',
-            link: 'https://github.com',
-            children: [],
-          },
-          // string - page file path
-          'bar.md', // resolved to `/foo/bar.md`
-          '/ray.md', // resolved to `/ray.md`
-        ],
-      },
-      // string - page file path
-      '/bar/README.md',
+  head: [
+    ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
+    [
+      "link",
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
     ],
-  }),
-
-  head: [['link', { rel: 'icon', href: '/images/logo-1.svg' }]],
-
-  bundler: viteBundler(),
-})
-
-
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;1,700&display=swap",
+        rel: "stylesheet",
+      },
+    ],
+  ],
+});
---
+++ docs/portfolio.md
@@ -0,0 +1,57 @@
+---
+home: true
+portfolio: true
+title: Portfolio Home
+icon: home
+name: Adrien Deprez
+avatar: 'https://avatars.githubusercontent.com/u/23341059'
+avatarStyle: 'align-self: center; border-radius: 100px; max-height: 40vh;'
+contents: 
+titles:
+  - Pragmatic developer
+  - Tech enthusiast
+  - Lifelong learner
+  - Engineer of things
+
+footer: false
+---
+
+## Who Am I ?
+
+This is a portfolio home page demo.
+
+To use this layout, you should set `home: true` and `portfolio: true` in the page front matter.
+
+For related configuration docs, please see [portfolio homepage](https://theme-hope.vuejs.press/guide/blog/home.html#portfolio-style-homepage).
+
+### My Story
+
+This is a portfolio home page demo.
+
+To use this layout, you should set `home: true` and `portfolio: true` in the page front matter.
+
+For related configuration docs, please see [portfolio homepage](https://theme-hope.vuejs.press/guide/blog/home.html#portfolio-style-homepage).
+
+### Experiences
+
+This is a portfolio home page demo.
+
+To use this layout, you should set `home: true` and `portfolio: true` in the page front matter.
+
+For related configuration docs, please see [portfolio homepage](https://theme-hope.vuejs.press/guide/blog/home.html#portfolio-style-homepage).
+
+### Technologies
+
+This is a portfolio home page demo.
+
+To use this layout, you should set `home: true` and `portfolio: true` in the page front matter.
+
+For related configuration docs, please see [portfolio homepage](https://theme-hope.vuejs.press/guide/blog/home.html#portfolio-style-homepage).
+
+### Where I'm Going
+
+This is a portfolio home page demo.
+
+To use this layout, you should set `home: true` and `portfolio: true` in the page front matter.
+
+For related configuration docs, please see [portfolio homepage](https://theme-hope.vuejs.press/guide/blog/home.html#portfolio-style-homepage).
